a556762e4b24e5dd8cd89fb608feb046a8bb55fe
shinya-nexseed <shirai@nexseed.net>
2017-04-12 15:36:42 +0800

    hogehoge修正

--- index.php
+++ index.php
@@ -1,1 +1,23 @@
-hogehoge
+<?php 
+  require('functions.php');
+// ロジック
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="utf-8">
+  <title></title>
+  <!-- CSSの読み込み -->
+  <?php require('load_css.php'); ?>
+</head>
+<body>
+ <?php require('header.php'); ?>
+ <?php require('left_side.php'); ?>
+ メインコンテンツ(index用)<br> <!-- これだけ残す -->
+ <?php require('footer.php'); ?>
+
+<!-- JavaScriptの読み込み -->
+ <?php require('load_js.php'); ?>
+</body>
+</html>
