a284b6db9c250e3810848dff0b0790a94a1206de
kensuke yoshida <kenban03@gmail.com>
2017-04-12 15:53:46 +0800

    絞り項目の作成

---
+++ design/search.php
@@ -0,0 +1,125 @@
+<div class ="filter">
+  <!-- 検索結果の項目 -->
+    <form class="form-horizontal">
+    <fieldset>
+
+    <!-- Form Name -->
+    <legend>絞り込み項目</legend>
+
+    <!-- Select Basic -->
+    <div class="form-group">
+      <label class="col-md-4 control-label" for="selectbasic">地域</label>
+      <div class="col-md-4">
+        <select name="selectbasic" class="form-control" id="selectbasic">
+          <option value="" selected>都道府県を選択</option>
+          <option value="1">北海道</option>
+          <option value="2">青森県</option>
+          <option value="3">岩手県</option>
+          <option value="4">宮城県</option>
+          <option value="5">秋田県</option>
+          <option value="6">山形県</option>
+          <option value="7">福島県</option>
+          <option value="8">茨城県</option>
+          <option value="9">栃木県</option>
+          <option value="10">群馬県</option>
+          <option value="11">埼玉県</option>
+          <option value="12">千葉県</option>
+          <option value="13">東京都</option>
+          <option value="14">神奈川県</option>
+          <option value="15">新潟県</option>
+          <option value="16">富山県</option>
+          <option value="17">石川県</option>
+          <option value="18">福井県</option>
+          <option value="19">山梨県</option>
+          <option value="20">長野県</option>
+          <option value="21">岐阜県</option>
+          <option value="22">静岡県</option>
+          <option value="23">愛知県</option>
+          <option value="24">三重県</option>
+          <option value="25">滋賀県</option>
+          <option value="26">京都府</option>
+          <option value="27">大阪府</option>
+          <option value="28">兵庫県</option>
+          <option value="29">奈良県</option>
+          <option value="30">和歌山県</option>
+          <option value="31">鳥取県</option>
+          <option value="32">島根県</option>
+          <option value="33">岡山県</option>
+          <option value="34">広島県</option>
+          <option value="35">山口県</option>
+          <option value="36">徳島県</option>
+          <option value="37">香川県</option>
+          <option value="38">愛媛県</option>
+          <option value="39">高知県</option>
+          <option value="40">福岡県</option>
+          <option value="41">佐賀県</option>
+          <option value="42">長崎県</option>
+          <option value="43">熊本県</option>
+          <option value="44">大分県</option>
+          <option value="45">宮崎県</option>
+          <option value="46">鹿児島県</option>
+          <option value="47">沖縄県</option>
+          </select>
+      </div>
+    </div>
+
+    <!-- Select Basic -->
+    <div class="form-group">
+      <div class="col-md-4">
+
+    <!-- Multiple Checkboxes -->
+    <div class="form-group">
+      <label class="col-md-4 control-label" for="checkboxes">犬のサイズ</label>
+      <div class="col-md-4">
+      <div class="checkbox">
+        <label for="checkboxes-0">
+          <input name="checkboxes" id="checkboxes-0" type="checkbox" value="">
+          小型犬
+        </label>
+        </div>
+      <div class="checkbox">
+        <label for="checkboxes-1">
+          <input name="checkboxes" id="checkboxes-1" type="checkbox" value="">
+          中型犬
+        </label>
+      </div>
+      <div class="checkbox">
+        <label for="checkboxes-2">
+          <input name="checkboxes" id="checkboxes-2" type="checkbox" value="">
+          大型犬
+        </label>
+      </div>
+      <div class="checkbox">
+        <label for="checkboxes-3">
+          <input name="checkboxes" id="checkboxes-3" type="checkbox" value="">
+          特大犬
+        </label>
+      </div>
+    
+
+    <!-- Button -->
+    <div class="form-group">
+      <label class="col-md-4 control-label" for="singlebutton"></label>
+      <div class="col-md-4">
+        <button name="singlebutton" class="btn btn-primary" id="singlebutton">OK</button>
+      </div>
+    </div>
+
+    </fieldset>
+    </form>
+
+</div>
+
+<div class= "result">
+  <!-- 検索結果の表示 -->
+      <div class "">
+      <!-- 検索結果数の表示 -->
+        
+      </div>
+      <div class = "view">
+          <div class =  "oneview">
+          <!-- 一つの検索項目 -->
+            
+          </div>
+      </div>
+</div>
