971d982bc4ee45dccd191c7c1547eed23f705663
saki <sakiandroi@icloud.com>
2017-04-18 14:19:00 +0800

    header修正

--- header.php
+++ header.php
@@ -2,22 +2,22 @@
   <nav>
     <ul>
       <li class="title">
-        <a href="#" style="font-size: 45px; font-family: 'Times New Roman',italic;">
+        <a href="top.html" style="font-size: 45px; font-family: 'Times New Roman',italic;">
           Life <span style="font-size:30px;">with</span> Dog
         </a>
       </li>
       <li class="nav_list">
-        <a href="#">
+        <a href="search_dog.html">
           体験したい人
         </a>
       </li>
       <li class="nav_list">
-        <a href="#">
+        <a href="search.html">
           体験する人
         </a>
       </li>
       <li class="nav_list">
-        <a href="#">
+        <a href="mypage.html">
           マイページ
         </a>
       </li>
