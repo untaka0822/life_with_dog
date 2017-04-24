43e3adf6efcc94abddc503f36ff13f793f16e356
saki <sakiandroi@icloud.com>
2017-04-24 15:43:44 +0800

    修正

--- header.php
+++ header.php
@@ -1,40 +1,40 @@
-<header>
- <nav>
-   <ul>
-     <li class=“title”>
-       <a href=“top.html” style=“font-size: 45px; font-family: ‘Times New Roman’,italic;“>
-         Life <span style=“font-size:30px;“>with</span> Dog
-       </a>
-     </li>
-     <li class=“nav_list”>
-       <a href=“search_dog.html”>
-         体験したい人
-       </a>
-     </li>
-     <li class=“nav_list”>
-       <a href=“search.html”>
-         預けたい人
-       </a>
-     </li>
-     <li class=“nav_list”>
-       <a href=“mypage.html”>
-         マイページ
-       </a>
-     </li>
-     <li class=“li-logout”>
-       <a href=“#”>
-         <div class=“hd-logout”>
-           Logout
-         </div>
-       </a>
-     </li>
-   </ul>
- </nav>
+<header class="navbar-fixed-top">
+  <nav>
+    <ul>
+      <li class="title">
+        <a href="#" style="font-size: 45px; font-family: 'Times New Roman',italic;">
+          Life <span style="font-size:30px;">with</span> Dog
+        </a>
+      </li>
+      <li class="nav_list">
+        <a href="#">
+          体験したい人
+        </a>
+      </li>
+      <li class="nav_list">
+        <a href="#">
+          体験する人
+        </a>
+      </li>
+      <li class="nav_list">
+        <a href="#">
+          マイページ
+        </a>
+      </li>
+      <li class="li-logout">
+        <a href="#">
+          <div class="hd-logout">
+            ログアウト
+          </div>
+        </a>
+      </li>
+    </ul>
+  </nav>
 </header>
 <div class=“clear”></div>
 <?php
  $file_name = getFileNameFromUri();
- if($file_name == ‘index.php’):
+ if($file_name == 'index.php'):
 ?>
 
 <?php endif; ?>
