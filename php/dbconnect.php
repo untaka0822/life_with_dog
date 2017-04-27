f721ef99e56624a6aea1aaa447a8f546db48bced
untaka0822 <untaka0822@gmail.com>
2017-04-27 22:48:13 +0800

    dbconnect.php 修正

--- php/dbconnect.php
+++ php/dbconnect.php
@@ -1,25 +1,11 @@
 
 <?php 
-<<<<<<< HEAD
-$dsh = 'mysql:dbname=life_with_dog;host=localhost';
-=======
 $dsn = 'mysql:dbname=life_with_dog;host=localhost';
->>>>>>> ed32b0d49da31dc9793944e118f27f0394be743d
 $user = 'root';
 $password = '';
-
-
-<<<<<<< HEAD
-$dbh = new PDO($dsh, $user, $password);
-$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-// PDOExceptionが使用可能になる。この中にエラー文が格納される
-$dbh->query('SET NAMES utf8');
-=======
 $dbh = new PDO($dsn, $user, $password);
 $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 // PDOExceptionが使用可能になる。この中にエラー文が格納される。
 $dbh->query('SET NAMES utf8');
 
-
->>>>>>> ed32b0d49da31dc9793944e118f27f0394be743d
 ?>
