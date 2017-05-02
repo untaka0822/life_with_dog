7af92db5ef17f67da618f1a947da11acc48ad07c
kensuke yoshida <kenban03@gmail.com>
2017-05-03 02:11:32 +0800

    ヘッダー追加

--- php/search.php
+++ php/search.php
@@ -199,33 +199,60 @@ if (!empty($_POST)) {
     <link href="../assets/css/main.css" rel="stylesheet">
     <link href="../assets/css/search_5star.css" rel="stylesheet">
   <title></title>
-  <header>
+  <header class="navbar-fixed-top">
   <nav>
     <ul>
       <li class="title">
-        <a href="top.html" style="font-size: 45px; font-family: 'Times New Roman',italic;">
+        <a href="top.php" style="font-size: 45px; font-family: 'Times New Roman',italic; position: relative;">
           Life <span style="font-size:30px;">with</span> Dog
         </a>
       </li>
-      <li class="nav_list">
-        <a href="#">
-          預けたい人
-        </a>
-      </li>
-      <li class="nav_list">
-        <a href="#">
+      <li class="nav_list" style="font-family: 'EB Garamond', serif;">
+        <a href="search_dog.php">
           体験したい人
         </a>
       </li>
-      <li class="nav_list">
-        <a href="mypage.html">
-          マイページ
+      <li class="nav_list" style="font-family: 'EB Garamond', serif;">
+        <a href="search.php">
+          預けたい人
         </a>
       </li>
-      <li class="li-logout">
+      <li class="nav_list" style="font-family: 'EB Garamond', serif;">
+        <a href="index_dog.php">
+          愛犬登録
+        </a>
+      </li>
+      <li class="nav_list" style="font-family: 'EB Garamond', serif;">
+        <div onclick="obj=document.getElementById('open').style; obj.display=(obj.display=='none')?'block':'none';">
+              <a style="cursor:pointer;">マイページ</a>
+            </div>
+
+            <div id="open" style="display:none;clear:both; z-index: 1;">
+
+              <div class="navbar navbar-default navbar-static-bottom" style="position: fixed; z-index: 1">
+                    <p class="navbar-text pull-left" style=": center;">
+                        <a href="mypage.php" target="_blank"> マイページ
+                    </p>
+                    <p class="navbar-text pull-left" style=": center;">
+                        <a href="custom.php" target="_blank"> 自分の情報を編集する
+                    </p>
+                    <p class="navbar-text pull-left" style=": center;">
+                        <a href="user_history.php" target="_blank"> 自分の利用履歴を見る
+                    </p>
+                    <p class="navbar-text pull-left" style=": center;">
+                        <a href="favorite_history.php" target="_blank"> 自分の気になる！履歴を見る
+                    </p>
+                    <p class="navbar-text pull-left" style=": center;">
+                        <a href="notice.php" target="_blank"> リクエスト一覧を見る
+                    </p>
+              </div>
+
+            </div>
+      </li>
+      <li class="li-logout" style="font-family: 'EB Garamond', serif;">
         <a href="#">
           <div class="hd-logout">
-            Logout
+            <a href="logout.php" style="color: white;">ログアウト</a>
           </div>
         </a>
       </li>
@@ -242,6 +269,8 @@ if (!empty($_POST)) {
 
     <!-- Form Name -->
      <div class="page-header  row">
+     <br>
+      <br>
      <h2 class="col-md-offset-1 col-md-6">預けたい人</h2>
      </div>
 
